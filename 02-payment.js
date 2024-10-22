2ca1e594db78537077bc991ef658dbd7cf0e85f6
Anna-1987 <mag260910@gmail.com>
2024-10-22 14:49:54 +0300

    cymma

---
+++ 02-payment.js
@@ -0,0 +1,17 @@
+
+// посчитать общую сумму покупок в карзине
+
+const cart = [54, 28, 105, 70, 92, 17, 120]
+let total = 0;
+// 1 перебрать массив
+// 2 сделать переменную total до цикла
+// 3 каждій елемент приплюсовать к total
+
+for (let i = 0; i < cart.length; i += 1) {
+    console.log(cart[i]);
+
+    total = total + cart[i];
+}
+    
+console.log("TOTAL =", total)
+
